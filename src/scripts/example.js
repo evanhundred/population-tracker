072a891573e2caa4e70498d8890f52a1c957bead
Evan Ryan <evan.hundred@gmail.com>
2022-11-11 08:33:28 -0500

    minor

--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,13 +1,13 @@
 class Example {
   constructor(ele) {
     this.ele = ele;
-    this.ele.innerHTML = "<h1>It's ALIVE!!!</h1>";
+    this.ele.innerHTML = "<h1>Population Tracker</h1>";
 
     this.ele.addEventListener("click", this.handleClick.bind(this));
   }
 
   handleClick() {
-    this.ele.children[0].innerText = "Ouch!";
+    this.ele.children[0].innerText = "...pOpUlAtIoN tRaCkEr...";
   }
 }
 
