33f742cb09630fbdca36e4bf92f3d7a422fd0b79
Evan Ryan <evan.hundred@gmail.com>
2022-11-12 06:08:48 -0500

    refactor data fetch

--- src/scripts/main-content.js
+++ src/scripts/main-content.js
@@ -6,26 +6,30 @@ class MainContent {
     this.ele.addEventListener("click", this.handleClick.bind(this));
   }
 
-  // sortData(data) {
-
-  // }
-
-  handleClick() {
-    let dataEl = document.createElement("data");
+  getData() {
     const dataBlock = require("/assets/2020_pop_by_state.json");
     const dataTitle = "2020 Census dataset";
-    // const dataBlock = require("/assets/sample_state_census_data.json");
-    const parsed_data = JSON.stringify(dataBlock);
+    const obj = {
+      header: dataTitle,
+      data: dataBlock,
+    };
+    return obj;
+  }
+
+  sortData(data) {}
+
+  handleClick() {
+    const dataObject = this.getData();
+    const dataHeader = dataObject.header;
+
     let ul = document.createElement("ul");
-    // let li = document.createElement("li");
-    // li.innerText = `State ID: Name`;
-    // ul.appendChild(li);
+    let dataEl = document.createElement("data");
 
     let first_entry_processed = false;
-    dataBlock.forEach((subArray) => {
+    dataObject.data.forEach((subArray) => {
       if (!first_entry_processed) {
         let li = document.createElement("li");
-        li.innerText = dataTitle;
+        li.innerText = dataHeader;
         li.classList.add("data-header");
         ul.appendChild(li);
         li = document.createElement("li");
