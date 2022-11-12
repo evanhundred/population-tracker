b24da642146f69b3393fc423de80b8fd604a4ae9
Evan Ryan <evan.hundred@gmail.com>
2022-11-11 20:10:51 -0500

    import unsorted 2020 census data: states and populations

--- src/scripts/main-content.js
+++ src/scripts/main-content.js
@@ -6,11 +6,38 @@ class MainContent {
     this.ele.addEventListener("click", this.handleClick.bind(this));
   }
 
+  // sortData(data) {
+
+  // }
+
   handleClick() {
     let dataEl = document.createElement("data");
-    const dataBlock = require("/assets/sample_state_census_data.json");
-    // dataEl.innerText = JSON.stringify(dataBlock, null, 4); // Indented 4 spaces
-    dataEl.innerText = JSON.stringify(dataBlock);
+    const dataBlock = require("/assets/2020_pop_by_state.json");
+    // const dataBlock = require("/assets/sample_state_census_data.json");
+    const parsed_data = JSON.stringify(dataBlock);
+    let ul = document.createElement("ul");
+    // let li = document.createElement("li");
+    // li.innerText = `State ID: Name`;
+    // ul.appendChild(li);
+
+    let first_entry_processed = false;
+    dataBlock.forEach((subArray) => {
+      if (!first_entry_processed) {
+        let li = document.createElement("li");
+        li.innerText = `State: Population`;
+        li.classList.add("first-item");
+        ul.appendChild(li);
+        first_entry_processed = true;
+      } else {
+        let li = document.createElement("li");
+        li.innerText = `${subArray[1]}: ${subArray[0]}`;
+        li.classList.add("item");
+        ul.appendChild(li);
+      }
+    });
+    dataEl.appendChild(ul);
+    // dataEl.innerText = JSON.stringify(dataBlock, null, "\t"); // Indented 4 spaces
+    // dataEl.innerText = JSON.stringify(dataBlock);
     this.ele.appendChild(dataEl);
     this.ele.children[0].innerText = "data fetched!";
   }
