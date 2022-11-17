41be2e4bc330d393e0ba4c3079c70e456914ae24
Evan Ryan <evan.hundred@gmail.com>
2022-11-17 14:53:36 -0500

    styling

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -8,9 +8,11 @@ class Footer {
     let contentEl = document.createElement("div");
     contentEl.setAttribute("id", "footer-content");
     contentEl.innerHTML =
-      'ER 2022 <a href="https://github.com/evanhundred" target="_blank">gh</a> <a href="https://www.linkedin.com/in/evan-ryan-1a2b07131/" target="_blank">li</a>';
+      '<p>ER 2022</p><a href="https://github.com/evanhundred" target="_blank"><img src="./assets/logos/github.png" alt="gh"/></a> <a href="https://www.linkedin.com/in/evan-ryan-1a2b07131/" target="_blank"><img src="./assets/logos/linkedin.png" alt="li"/></a>';
     container.appendChild(contentEl);
   }
 }
 
 export default Footer;
+
+// 'ER 2022 <a href="https://github.com/evanhundred" target="_blank">gh</a> <a href="https://www.linkedin.com/in/evan-ryan-1a2b07131/" target="_blank">li</a>';
