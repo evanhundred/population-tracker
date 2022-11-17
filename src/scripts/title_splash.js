3ea4502e36f36794e46d9f1b2f72a3661017fe0f
Evan Ryan <evan.hundred@gmail.com>
2022-11-17 18:36:07 -0500

    egg

--- src/scripts/title_splash.js
+++ src/scripts/title_splash.js
@@ -1,13 +1,25 @@
 class TitleSplash {
   constructor(ele) {
     this.ele = ele;
-    this.ele.innerHTML = "<h1>Population Tracker</h1>";
+    this.ele.innerHTML = "<h1>Population Track<span class='egg'>e</span>r</h1>";
+    this.textStyle = "normal";
 
     this.ele.addEventListener("click", this.handleClick.bind(this));
   }
 
-  handleClick() {
-    this.ele.children[0].innerText = "...pOpUlAtIoN tRaCkEr...";
+  handleClick(e) {
+    let eventTarget = e.target;
+    if (eventTarget.classList.contains("egg")) {
+      if (this.textStyle === "normal") {
+        this.textStyle = "spooky";
+        this.ele.children[0].innerHTML =
+          "<h1>...pOpUl<span class='egg'>A</span>tIoN tRaCkEr...</h1>";
+      } else {
+        this.textStyle = "normal";
+        this.ele.children[0].innerHTML =
+          "<h1>Population Track<span class='egg'>e</span>r</h1>";
+      }
+    }
   }
 }
 
