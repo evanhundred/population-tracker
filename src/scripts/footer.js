778f54e2fd54a7d69893b010b25a909e6c243256
Evan Ryan <evan.hundred@gmail.com>
2023-01-04 06:04:11 -0500

    fix refactor error, clean up for main merge

--- src/scripts/footer.js
+++ src/scripts/footer.js
@@ -8,7 +8,7 @@ class Footer {
     let contentEl = document.createElement("div");
     contentEl.setAttribute("id", "footer-content");
     contentEl.innerHTML =
-      '<p>ER 2022</p><a href="https://github.com/evanhundred" target="_blank"><img src="./assets/logos/github.png" alt="gh"/></a> <a href="https://www.linkedin.com/in/evan-ryan-1a2b07131/" target="_blank"><img src="./assets/logos/linkedin.png" alt="li"/></a>';
+      '<p>ER 2023</p><a href="https://github.com/evanhundred" target="_blank"><img src="./assets/logos/github.png" alt="gh"/></a> <a href="https://www.linkedin.com/in/evan-ryan-1a2b07131/" target="_blank"><img src="./assets/logos/linkedin.png" alt="li"/></a>';
     container.appendChild(contentEl);
   }
 }
