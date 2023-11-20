03828e7cd4dbebc4b2300a65297dfd314b3870b6
Evan Ryan <evan.hundred@gmail.com>
2023-11-20 09:53:41 -0500

    implement pop to color range calculator function in map

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -19,73 +19,96 @@ class Map {
   }
 
   styleMap(data) {
-    // debugger;
+    const lowColorPct = 0.4;
+    let range = 1 - lowColorPct;
+    const getPct = (level) => {
+      const res = (level / 29) * range;
+      // const res = (range / 29) * level;
+      return res + lowColorPct;
+    };
+    let colorBase = "rgba(222, 31, 18, ";
+    let colorLevel;
+    // let getColorLevel = (num = 0) => {
+    //   return num + 1;
+    // };
+
+    const popDegreesArray = [
+      [50000, "under50k"],
+      [200000, "under200k"],
+      [500000, "under500k"],
+      [1000000, "under1m"],
+      [1500000, "under1p5m"],
+      [2000000, "under2m"],
+      [2500000, "under2p5m"],
+      [3000000, "under3m"],
+      [3500000, "under3p5m"],
+      [4000000, "under4m"],
+      [4500000, "under4p5m"],
+      [5000000, "under5m"],
+      [5500000, "under5p5m"],
+      [6000000, "under6m"],
+      [6500000, "under6p5m"],
+      [7000000, "under7m"],
+      [7500000, "under7p5m"],
+      [8000000, "under8m"],
+      [9000000, "under9m"],
+      [10000000, "under10m"],
+      [11000000, "under11m"],
+      [12000000, "under12m"],
+      [13000000, "under13m"],
+      [14000000, "under14m"],
+      [15000000, "under15m"],
+      [20000000, "under20m"],
+      [25000000, "under25m"],
+      [30000000, "under30m"],
+      [35000000, "under35m"],
+      [50000000, "under50m"]
+    ];
+
+    const popDegrees = {};
+    popDegreesArray.forEach((degree, idx) => {
+      popDegrees[idx] = {
+        integer: degree[0],
+        className: degree[1]
+      };
+    });
+    console.log(popDegrees);
+
     d3.selectAll(".state")._groups[0].forEach((ele) => {
+      // console.log(ele);
       ele.classList.remove(...ele.classList);
       let state = ele.__data__.properties.name;
-      if (data[state].population < 50000) {
-        ele.classList.add("under50k");
-      } else if (data[state].population < 200000) {
-        ele.classList.add("under200k");
-      } else if (data[state].population < 500000) {
-        ele.classList.add("under500k");
-      } else if (data[state].population < 1000000) {
-        ele.classList.add("under1m");
-      } else if (data[state].population < 1500000) {
-        ele.classList.add("under1p5m");
-      } else if (data[state].population < 2000000) {
-        ele.classList.add("under2m");
-      } else if (data[state].population < 2500000) {
-        ele.classList.add("under2p5m");
-      } else if (data[state].population < 3000000) {
-        ele.classList.add("under3m");
-      } else if (data[state].population < 3500000) {
-        ele.classList.add("under3p5m");
-      } else if (data[state].population < 4000000) {
-        ele.classList.add("under4m");
-      } else if (data[state].population < 4500000) {
-        ele.classList.add("under4p5m");
-      } else if (data[state].population < 5000000) {
-        ele.classList.add("under5m");
-      } else if (data[state].population < 5500000) {
-        ele.classList.add("under5p5m");
-      } else if (data[state].population < 6000000) {
-        ele.classList.add("under6m");
-      } else if (data[state].population < 6500000) {
-        ele.classList.add("under6p5m");
-      } else if (data[state].population < 7000000) {
-        ele.classList.add("under7m");
-      } else if (data[state].population < 7500000) {
-        ele.classList.add("under7p5m");
-      } else if (data[state].population < 8000000) {
-        ele.classList.add("under8m");
-      } else if (data[state].population < 9000000) {
-        ele.classList.add("under9m");
-      } else if (data[state].population < 10000000) {
-        ele.classList.add("under10m");
-      } else if (data[state].population < 11000000) {
-        ele.classList.add("under11m");
-      } else if (data[state].population < 12000000) {
-        ele.classList.add("under12m");
-      } else if (data[state].population < 13000000) {
-        ele.classList.add("under13m");
-      } else if (data[state].population < 14000000) {
-        ele.classList.add("under14m");
-      } else if (data[state].population < 15000000) {
-        ele.classList.add("under15m");
-      } else if (data[state].population < 20000000) {
-        ele.classList.add("under20m");
-      } else if (data[state].population < 25000000) {
-        ele.classList.add("under25m");
-      } else if (data[state].population < 30000000) {
-        ele.classList.add("under30m");
-      } else if (data[state].population < 35000000) {
-        ele.classList.add("under15m");
-      } else if (data[state].population < 50000000) {
-        ele.classList.add("under50m");
-      }
-    }); // nodeList of path els
-    // debugger;
+
+      console.log(state);
+      console.log(data[state]);
+      8;
+      let currentPop = data[state].population;
+
+      const findDegreeIdx = () => {
+        const degrees = popDegreesArray.slice();
+        let idx = 0;
+        while (degrees) {
+          let currentDegree = degrees.shift()[0];
+          // console.log(currentDegree);
+          if (currentPop > currentDegree) {
+            idx += 1;
+          } else {
+            return idx;
+          }
+        }
+        return null;
+      };
+
+      colorLevel = findDegreeIdx();
+      console.log(colorLevel);
+
+      let colorPctString = getPct(colorLevel).toString();
+      let colorString = `${colorBase}${colorPctString})`;
+      console.log(colorString);
+      ele.style.fill = colorString;
+      ele.style.color = colorString;
+      ele.classList.add(popDegrees[colorLevel].className);
+    });
   }
 
   renderMap() {
--- src/scripts/printer/index.js
+++ src/scripts/printer/index.js
@@ -42,7 +42,7 @@ class Printer {
     // let dataObject = this.sortData("byName");
     // ;
     console.log(newDataObject);
-    const dataObject = newDataObject;
+    let dataObject = newDataObject;
     console.log(dataObject);
     let dataHeader = dataObject.header;
     let dataEl = document.querySelector("data");
@@ -133,100 +133,100 @@ class Printer {
     ul.appendChild(totalUSPop);
   }
 
-  sortByPopulation(newDataObject = null) {
-    if (newDataObject) {
-      this.dataObject = newDataObject;
-    } else {
-      newDataObject = this.dataObject;
-    }
+  // sortByPopulation(newDataObject = null) {
+  //   if (newDataObject) {
+  //     this.dataObject = newDataObject;
+  //   } else {
+  //     newDataObject = this.dataObject;
+  //   }
 
-    let addPopClass = new AddPopClass();
-    console.log(newDataObject);
-    // this.sortStyle = "byPop";
-    let dataObject = newDataObject;
-    let dataHeader = dataObject.header;
-    let dataEl = document.querySelector("data");
-    console.log(dataEl);
-    let ul = document.createElement("ul");
-    dataEl.replaceChild(ul, document.querySelector(".fetchResultSorted"));
-    ul.classList.add("fetchResultSorted");
+  //   let addPopClass = new AddPopClass();
+  //   console.log(newDataObject);
+  //   // this.sortStyle = "byPop";
+  //   let dataObject = newDataObject;
+  //   let dataHeader = dataObject.header;
+  //   let dataEl = document.querySelector("data");
+  //   console.log(dataEl);
+  //   let ul = document.createElement("ul");
+  //   dataEl.replaceChild(ul, document.querySelector(".fetchResultSorted"));
+  //   ul.classList.add("fetchResultSorted");
 
-    let fiftyPlusDCName;
-    let fiftyPlusDCPop;
-    let totalUSName;
-    let totalUSPop;
-    let totalTerrName;
-    let totalTerrPop;
+  //   let fiftyPlusDCName;
+  //   let fiftyPlusDCPop;
+  //   let totalUSName;
+  //   let totalUSPop;
+  //   let totalTerrName;
+  //   let totalTerrPop;
 
-    let vintageLabel = document.createElement("li");
-    vintageLabel.innerText = dataHeader;
-    vintageLabel.classList.add("data-header");
-    ul.appendChild(vintageLabel);
+  //   let vintageLabel = document.createElement("li");
+  //   vintageLabel.innerText = dataHeader;
+  //   vintageLabel.classList.add("data-header");
+  //   ul.appendChild(vintageLabel);
 
-    let sortLabel = document.createElement("li");
-    sortLabel.innerText = "by population";
-    sortLabel.classList = "sortLabel";
-    ul.appendChild(sortLabel);
+  //   let sortLabel = document.createElement("li");
+  //   sortLabel.innerText = "by population";
+  //   sortLabel.classList = "sortLabel";
+  //   ul.appendChild(sortLabel);
 
-    let keyState = document.createElement("li");
-    keyState.classList.add("keyState");
-    keyState.innerText = "State";
-    ul.appendChild(keyState);
-    let keyPop = document.createElement("li");
-    keyPop.classList.add("keyPop");
-    keyPop.innerText = "Population";
-    ul.appendChild(keyPop);
+  //   let keyState = document.createElement("li");
+  //   keyState.classList.add("keyState");
+  //   keyState.innerText = "State";
+  //   ul.appendChild(keyState);
+  //   let keyPop = document.createElement("li");
+  //   keyPop.classList.add("keyPop");
+  //   keyPop.innerText = "Population";
+  //   ul.appendChild(keyPop);
 
-    for (let i = 0; i < dataObject.states.length; i++) {
-      if (
-        dataObject.states[i].stateName !== "50 states + DC" &&
-        dataObject.states[i].stateName !== "Total territories" &&
-        dataObject.states[i].stateName !== "Total U.S. population"
-      ) {
-        // ;
-        let stateName = document.createElement("li");
-        stateName.classList.add("item", "itemName");
-        addPopClass.addClass(dataObject.states[i], stateName);
-        stateName.innerText = `${dataObject.states[i].stateName}`;
-        ul.appendChild(stateName);
-        let statePop = document.createElement("li");
-        statePop.classList.add("item", "itemPop");
-        addPopClass.addClass(dataObject.states[i], statePop);
-        statePop.innerText = `${dataObject.states[i].population}`;
-        ul.appendChild(statePop);
-      } else if (dataObject.states[i].stateName === "50 states + DC") {
-        fiftyPlusDCName = document.createElement("li");
-        fiftyPlusDCName.classList.add("item", "itemName", "fiftyPlusDC");
-        fiftyPlusDCName.innerText = `${dataObject.states[i].stateName}`;
-        fiftyPlusDCPop = document.createElement("li");
-        fiftyPlusDCPop.classList.add("item", "itemPop", "fiftyPlusDC");
-        // addPopClass.addClass(dataObject.states[i], fiftyPlusDCPop);
-        fiftyPlusDCPop.innerText = `${dataObject.states[i].population}`;
-      } else if (dataObject.states[i].stateName === "Total U.S. population") {
-        totalUSName = document.createElement("li");
-        totalUSName.classList.add("item", "itemName", "totalUS");
-        totalUSName.innerText = `${dataObject.states[i].stateName}`;
-        totalUSPop = document.createElement("li");
-        // addPopClass.addClass(dataObject.states[i], totalUSPop);
-        totalUSPop.classList.add("item", "itemPop", "totalUS");
-        totalUSPop.innerText = `${dataObject.states[i].population}`;
-      } else if (dataObject.states[i].stateName === "Total territories") {
-        totalTerrName = document.createElement("li");
-        totalTerrName.classList.add("item", "itemName", "totalTerr");
-        totalTerrName.innerText = `${dataObject.states[i].stateName}`;
-        totalTerrPop = document.createElement("li");
-        totalTerrPop.classList.add("item", "itemPop", "totalTerr");
-        // addPopClass.addClass(dataObject.states[i], totalTerrPop);
-        totalTerrPop.innerText = `${dataObject.states[i].population}`;
-      }
-    }
-    ul.appendChild(totalTerrName);
-    ul.appendChild(totalTerrPop);
-    ul.appendChild(fiftyPlusDCName);
-    ul.appendChild(fiftyPlusDCPop);
-    ul.appendChild(totalUSName);
-    ul.appendChild(totalUSPop);
-  }
+  //   for (let i = 0; i < dataObject.states.length; i++) {
+  //     if (
+  //       dataObject.states[i].stateName !== "50 states + DC" &&
+  //       dataObject.states[i].stateName !== "Total territories" &&
+  //       dataObject.states[i].stateName !== "Total U.S. population"
+  //     ) {
+  //       // ;
+  //       let stateName = document.createElement("li");
+  //       stateName.classList.add("item", "itemName");
+  //       addPopClass.addClass(dataObject.states[i], stateName);
+  //       stateName.innerText = `${dataObject.states[i].stateName}`;
+  //       ul.appendChild(stateName);
+  //       let statePop = document.createElement("li");
+  //       statePop.classList.add("item", "itemPop");
+  //       addPopClass.addClass(dataObject.states[i], statePop);
+  //       statePop.innerText = `${dataObject.states[i].population}`;
+  //       ul.appendChild(statePop);
+  //     } else if (dataObject.states[i].stateName === "50 states + DC") {
+  //       fiftyPlusDCName = document.createElement("li");
+  //       fiftyPlusDCName.classList.add("item", "itemName", "fiftyPlusDC");
+  //       fiftyPlusDCName.innerText = `${dataObject.states[i].stateName}`;
+  //       fiftyPlusDCPop = document.createElement("li");
+  //       fiftyPlusDCPop.classList.add("item", "itemPop", "fiftyPlusDC");
+  //       // addPopClass.addClass(dataObject.states[i], fiftyPlusDCPop);
+  //       fiftyPlusDCPop.innerText = `${dataObject.states[i].population}`;
+  //     } else if (dataObject.states[i].stateName === "Total U.S. population") {
+  //       totalUSName = document.createElement("li");
+  //       totalUSName.classList.add("item", "itemName", "totalUS");
+  //       totalUSName.innerText = `${dataObject.states[i].stateName}`;
+  //       totalUSPop = document.createElement("li");
+  //       // addPopClass.addClass(dataObject.states[i], totalUSPop);
+  //       totalUSPop.classList.add("item", "itemPop", "totalUS");
+  //       totalUSPop.innerText = `${dataObject.states[i].population}`;
+  //     } else if (dataObject.states[i].stateName === "Total territories") {
+  //       totalTerrName = document.createElement("li");
+  //       totalTerrName.classList.add("item", "itemName", "totalTerr");
+  //       totalTerrName.innerText = `${dataObject.states[i].stateName}`;
+  //       totalTerrPop = document.createElement("li");
+  //       totalTerrPop.classList.add("item", "itemPop", "totalTerr");
+  //       // addPopClass.addClass(dataObject.states[i], totalTerrPop);
+  //       totalTerrPop.innerText = `${dataObject.states[i].population}`;
+  //     }
+  //   }
+  //   ul.appendChild(totalTerrName);
+  //   ul.appendChild(totalTerrPop);
+  //   ul.appendChild(fiftyPlusDCName);
+  //   ul.appendChild(fiftyPlusDCPop);
+  //   ul.appendChild(totalUSName);
+  //   ul.appendChild(totalUSPop);
+  // }
 
   sortData(sortKey) {
     const preSorted = {};
