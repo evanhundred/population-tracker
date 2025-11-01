0d9b2ba533834a61fddafb0f279ff642ee2e7d2f
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2025-10-31 20:53:10 -0400

    add import statements for d3 to Map; create gemini preferences file

---
+++ .gemini/GEMINI.md
@@ -0,0 +1,5 @@
+# Project: Population Tracker
+
+## General Instructions
+
+- When you have a code addition, change, or removal to recommend, please display the recommended change or code diff, without asking to make the change yourself. At this point I prefer to make the change myself.
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -2,6 +2,8 @@ import StateData from '/assets/states-albers-10m.json';
 import State from './State';
 import Footer from './Footer';
 import { popDegreesArray, LOW_COLOR_PCT } from './util';
+import * as d3 from 'd3';
+import * as topojson from 'topojson-client';
 
 class Map {
   constructor(data = null) {
