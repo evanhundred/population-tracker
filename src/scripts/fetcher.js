51dc1d237f2dd90607e2dc37787c4e672660d031
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2025-11-06 13:30:46 -0500

    middle of refactor

--- src/scripts/fetcher.js
+++ src/scripts/fetcher.js
@@ -1,5 +1,5 @@
-import Map from './Map';
-import Printer from './Printer';
+// import Map from './Map';
+// import Printer from './Printer';
 import { sortData, resetDataObject } from './util';
 
 class Fetcher {
@@ -9,55 +9,53 @@ class Fetcher {
   }
 
   fetchData(vintage) {
-    this.getData(vintage);
-    return this.dataObject;
+    // this.getData(vintage);
+    // return this.dataObject;
+    return this.getData(vintage);
   }
 
-  getData(vintage) {
+  async getData(vintage) {
     this.dataObject = resetDataObject(vintage);
-    // console.log(this.dataObject);
-    const resetSuccessMessage = () => {
-      let firstLineFooterH2 = document.getElementById('firstLineFooterH2');
-      firstLineFooterH2.innerText = 'fetching...';
-    };
-    resetSuccessMessage();
+    // const resetSuccessMessage = () => {
+    //   let firstLineFooterH2 = document.getElementById('firstLineFooterH2');
+    //   firstLineFooterH2.innerText = 'fetching...';
+    // };
+    // resetSuccessMessage();
 
-    // this.dataObject;
     let url;
 
-    const resetMap = () => {
-      let mapDiv = document.querySelector('#map');
-      if (mapDiv) mapDiv.remove();
-      mapDiv = document.createElement('div');
-      mapDiv.setAttribute('id', 'map');
-      const main = document.getElementById('main-content');
-      const secondLine = document.getElementById('second-line');
-      main.insertBefore(mapDiv, secondLine);
-    };
+    // const resetMap = () => {
+    //   let mapDiv = document.querySelector('#map');
+    //   if (mapDiv) mapDiv.remove();
+    //   mapDiv = document.createElement('div');
+    //   mapDiv.setAttribute('id', 'map');
+    //   const main = document.getElementById('main-content');
+    //   const secondLine = document.getElementById('second-line');
+    //   main.insertBefore(mapDiv, secondLine);
+    // };
 
-    const loadAndPrint = (responseText = null) => {
-      let data;
-      if (responseText) {
-        data = JSON.parse(responseText);
-        this.dataObject.data = data;
-      } else {
-        // this.dataObject.localData
-      }
-      this.dataObject.header = this.dataObject.dataTitle;
+    // const loadAndPrint = (responseText = null) => {
+    //   let data;
+    //   if (responseText) {
+    //     data = JSON.parse(responseText);
+    //     this.dataObject.data = data;
+    //   } else {
+    //     // this.dataObject.localData
+    //   }
+    //   this.dataObject.header = this.dataObject.dataTitle;
 
-      this.loadLocalData(vintage);
-      // console.log(this.dataObject);
-      this.dataObject = sortData(this.dataObject, this.sortStyle);
-      resetMap();
+    //   this.loadLocalData(vintage);
+    //   this.dataObject = sortData(this.dataObject, this.sortStyle);
+    //   resetMap();
 
-      new Map(this.dataObject);
+    //   new Map(this.dataObject);
 
-      const printer = new Printer(this.dataObject, this.sortStyle);
-      printer.printData();
-      printer.sortByName(this.dataObject, this.sortStyle);
+    //   const printer = new Printer(this.dataObject, this.sortStyle);
+    //   printer.printData();
+    //   printer.sortByName(this.dataObject, this.sortStyle);
 
-      return this.dataObject;
-    };
+    //   return this.dataObject;
+    // };
 
     switch (vintage) {
       case '2020':
@@ -74,40 +72,60 @@ class Fetcher {
         break;
       case '1790':
         this.dataObject.dataTitle = '1790 Census dataset';
-        loadAndPrint();
-        return;
+        // loadAndPrint();
+        // return;
+        this.loadLocalData(vintage);
+        this.dataObject = sortData(this.dataObject, this.sortStyle);
+        return this.dataObject;
       default:
         break;
     }
 
-    const request = new XMLHttpRequest();
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      this.dataObject.data = data;
+      this.dataObject.header = this.dataObject.dataTitle;
+      this.loadLocalData(vintage);
+      this.dataObject = sortData(this.dataObject, this.sortStyle);
+      return this.dataObject;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return { errors: 'Error fetching data' };
+    }
 
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        return loadAndPrint(request.responseText);
-      }
-      // return this.dataObject;
-      return { errors: 'readystatechange' };
-    });
+    // const request = new XMLHttpRequest();
 
-    request.open('GET', url);
-    request.send();
+    // request.addEventListener('readystatechange', () => {
+    //   if (request.readyState === 4 && request.status === 200) {
+    //     return loadAndPrint(request.responseText);
+    //   }
+    //   // return this.dataObject;
+    //   return { errors: 'readystatechange' };
+    // });
+
+    // request.open('GET', url);
+    // request.send();
   }
 
-  loadLocalData(vintage) {
+  async loadLocalData(vintage) {
     let dataBlock;
     switch (vintage) {
       case '2020':
-        dataBlock = require('/assets/territories-2020.json');
+        dataBlock = (await import('/assets/terretories-2020.json')).default;
+        // dataBlock = require('/assets/territories-2020.json');
         break;
       case '2010':
-        dataBlock = require('/assets/territories-2010.json');
+        dataBlock = (await import('/assets/terretories-2010.json')).default;
+        // dataBlock = require('/assets/territories-2010.json');
         break;
       case '2000':
-        dataBlock = require('/assets/territories-2000.json');
+        dataBlock = (await import('/assets/terretories-2000.json')).default;
+        // dataBlock = require('/assets/territories-2000.json');
         break;
       case '1790':
-        dataBlock = require('/assets/historical-data/1790.json');
+        dataBlock = (await import('/assets/historical-data/1790.json')).default;
+        // dataBlock = require('/assets/historical-data/1790.json');
         break;
     }
     this.dataObject.localData = dataBlock;
