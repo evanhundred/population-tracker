2fbc3814a9640155cdf2eafdeef547773d9289d9
Evan Ryan <107318423+evanhundred@users.noreply.github.com>
2025-11-10 17:38:34 -0500

    shell script works, but json formatting is incorrect

--- assets/parse.sh
+++ assets/parse.sh
@@ -11,14 +11,32 @@
 for index in {5..32}
 do
     echo "["
-    # IFS="," read -r stateNameTitle stateNumberTitle censusTitle
-    # echo "[\"NAME\", \"$censusTitle\", \"state\"],"
+
+    first_line=true
     while IFS="," read -r stateName stateNumber population
     do
-        echo "[$stateName, \"$population\", $stateNumber],"
+        if [ "$stateName" = "STATE" ]; then
+            echo "    [\"NAME\", \"$population\", \"state\"],"
+            continue
+        fi
+        if [ -z "$stateNumber" ]; then
+            stateNumber="\"\""
+        fi
+        if [ -z "$population" ]; then
+            population=""
+        fi
+
+        if [ "$first_line" = true ]; then
+            first_line=false
+        else
+            echo ","
+        fi
+        echo -n "    [$stateName, \"$population\", $stateNumber]"
     done < <(cut -d "," -f2,3,$index census-time-table.csv)
-    # index = index + 1
-    if [ $index -lt 28 ]; then
+
+    echo ""
+
+    if [ $index -lt 32 ]; then
         echo "],"
     else
         echo "]"
